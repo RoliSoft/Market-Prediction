39897a60e958f5f101a6980ed932b292122ffc96
RoliSoft <rolisoft@gmail.com>
2015-12-15 11:39:45 +0200

    Added charting.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace MarketPrediction
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panelChart = new System.Windows.Forms.Panel();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.panelControl = new System.Windows.Forms.Panel();
+            this.comboBoxSeries = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.panelChart.SuspendLayout();
+            this.panelControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart
+            // 
+            this.chart.BackColor = System.Drawing.SystemColors.Control;
+            chartArea2.AxisX.InterlacedColor = System.Drawing.Color.Silver;
+            chartArea2.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea2.AxisX.IsStartedFromZero = false;
+            chartArea2.AxisX.LineColor = System.Drawing.Color.Silver;
+            chartArea2.AxisX.MajorGrid.LineColor = System.Drawing.Color.Silver;
+            chartArea2.AxisX.MajorTickMark.LineColor = System.Drawing.Color.Silver;
+            chartArea2.AxisX.ScaleBreakStyle.LineColor = System.Drawing.Color.Silver;
+            chartArea2.AxisY.InterlacedColor = System.Drawing.Color.Silver;
+            chartArea2.AxisY.IsStartedFromZero = false;
+            chartArea2.AxisY.LineColor = System.Drawing.Color.Silver;
+            chartArea2.AxisY.MajorGrid.LineColor = System.Drawing.Color.Silver;
+            chartArea2.AxisY.MajorTickMark.LineColor = System.Drawing.Color.Silver;
+            chartArea2.AxisY.ScaleBreakStyle.LineColor = System.Drawing.Color.Silver;
+            chartArea2.BackColor = System.Drawing.SystemColors.Control;
+            chartArea2.BorderColor = System.Drawing.Color.Silver;
+            chartArea2.IsSameFontSizeForAllAxes = true;
+            chartArea2.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea2);
+            this.chart.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend2.Alignment = System.Drawing.StringAlignment.Center;
+            legend2.BackColor = System.Drawing.SystemColors.Control;
+            legend2.BorderColor = System.Drawing.Color.Transparent;
+            legend2.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Top;
+            legend2.LegendStyle = System.Windows.Forms.DataVisualization.Charting.LegendStyle.Row;
+            legend2.Name = "Legend1";
+            legend2.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chart.Legends.Add(legend2);
+            this.chart.Location = new System.Drawing.Point(0, 0);
+            this.chart.Name = "chart";
+            this.chart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            this.chart.Size = new System.Drawing.Size(702, 426);
+            this.chart.TabIndex = 0;
+            // 
+            // panelChart
+            // 
+            this.panelChart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelChart.Controls.Add(this.chart);
+            this.panelChart.Location = new System.Drawing.Point(0, 0);
+            this.panelChart.Name = "panelChart";
+            this.panelChart.Size = new System.Drawing.Size(702, 426);
+            this.panelChart.TabIndex = 1;
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(149, 14);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoad.TabIndex = 2;
+            this.buttonLoad.Text = "Load";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // panelControl
+            // 
+            this.panelControl.Controls.Add(this.comboBoxSeries);
+            this.panelControl.Controls.Add(this.buttonLoad);
+            this.panelControl.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelControl.Location = new System.Drawing.Point(0, 419);
+            this.panelControl.Name = "panelControl";
+            this.panelControl.Size = new System.Drawing.Size(702, 48);
+            this.panelControl.TabIndex = 3;
+            // 
+            // comboBoxSeries
+            // 
+            this.comboBoxSeries.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxSeries.FormattingEnabled = true;
+            this.comboBoxSeries.Location = new System.Drawing.Point(12, 15);
+            this.comboBoxSeries.Name = "comboBoxSeries";
+            this.comboBoxSeries.Size = new System.Drawing.Size(131, 21);
+            this.comboBoxSeries.TabIndex = 4;
+            this.comboBoxSeries.SelectedIndexChanged += new System.EventHandler(this.comboBoxSeries_SelectedIndexChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 467);
+            this.Controls.Add(this.panelControl);
+            this.Controls.Add(this.panelChart);
+            this.Name = "MainForm";
+            this.Text = "Market Prediction";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.panelChart.ResumeLayout(false);
+            this.panelControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.Panel panelChart;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Panel panelControl;
+        private System.Windows.Forms.ComboBox comboBoxSeries;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,147 @@
+﻿namespace MarketPrediction
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Windows.Forms;
+    using System.Windows.Forms.DataVisualization.Charting;
+
+    /// <summary>
+    /// Main form of the application.
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// Contains the list of loaded indices.
+        /// </summary>
+        private Dictionary<string, Dictionary<DateTime, decimal>> indices;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainForm"/> class.
+        /// </summary>
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles the Load event of the MainForm control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                indices = OandaReader.ReadIndices("oanda_2014-2015.csv");
+                comboBoxSeries.Items.AddRange(indices.Keys.ToArray<object>());
+                comboBoxSeries.SelectedIndex = 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to load indices: " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Handles the SelectedIndexChanged event of the comboBoxSeries control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void comboBoxSeries_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            buttonLoad.Text = chart.Series.Any(x => x.Name == (string)comboBoxSeries.SelectedItem) ? "Unload" : "Load";
+        }
+
+        /// <summary>
+        /// Handles the Click event of the buttonLoad control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            if (comboBoxSeries.SelectedItem == null)
+            {
+                return;
+            }
+
+            if (chart.Series.Any(x => x.Name == (string)comboBoxSeries.SelectedItem))
+            {
+                // unload data
+
+                chart.Series.Remove(chart.Series.First(x => x.Name == (string)comboBoxSeries.SelectedItem));
+                comboBoxSeries_SelectedIndexChanged(buttonLoad, e);
+            }
+            else
+            {
+                // load data
+
+                Dictionary<DateTime, decimal> index;
+                if (indices.TryGetValue((string)comboBoxSeries.SelectedItem, out index))
+                {
+                    loadSeries((string)comboBoxSeries.SelectedItem, index);
+                    comboBoxSeries_SelectedIndexChanged(buttonLoad, e);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Loads the series into the main chart.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="indices">The indices.</param>
+        private void loadSeries(string name, Dictionary<DateTime,decimal> indices)
+        {
+            var series = new Series(name);
+            series.ChartType = SeriesChartType.FastLine;
+
+            foreach (var index in indices)
+            {
+                series.Points.AddXY(index.Key, index.Value);
+            }
+
+            try
+            {
+                chart.Series.Add(series);
+            }
+            catch (ArgumentException)
+            {
+                chart.Series.Remove(chart.Series.FirstOrDefault(x => x.Name == name));
+                chart.Series.Add(series);
+            }
+
+            chart.ChartAreas[0].AxisY.Minimum = Math.Min(chart.ChartAreas[0].AxisY.Minimum, series.Points.FindMinByValue("Y1").GetValueByName("Y1"));
+            chart.ChartAreas[0].AxisY.Maximum = Math.Max(chart.ChartAreas[0].AxisY.Maximum, series.Points.FindMaxByValue("Y1").GetValueByName("Y1"));
+        }
+
+        /// <summary>
+        /// Loads the series into the main chart.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="indices">The indices.</param>
+        private void loadSeries(string name, IEnumerable<decimal> indices)
+        {
+            var series = new Series(name);
+            series.ChartType = SeriesChartType.FastLine;
+
+            foreach (var index in indices)
+            {
+                series.Points.AddY(index);
+            }
+
+            try
+            {
+                chart.Series.Add(series);
+            }
+            catch (ArgumentException)
+            {
+                chart.Series.Remove(chart.Series.FirstOrDefault(x => x.Name == name));
+                chart.Series.Add(series);
+            }
+
+            chart.ChartAreas[0].AxisY.Minimum = Math.Min(chart.ChartAreas[0].AxisY.Minimum, series.Points.FindMinByValue("Y1").GetValueByName("Y1"));
+            chart.ChartAreas[0].AxisY.Maximum = Math.Max(chart.ChartAreas[0].AxisY.Maximum, series.Points.FindMaxByValue("Y1").GetValueByName("Y1"));
+        }
+    }
+}
